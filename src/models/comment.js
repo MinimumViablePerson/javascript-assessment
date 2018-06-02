30ef780ee75030f7d76a1d9450dd46878b36f8f8
NMarcora <nicolas.marcora@yahoo.co.uk>
2018-06-02 21:30:33 +0100

    change indentation to 2 spaces for consistency

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,25 +1,25 @@
 class Comment {
-    constructor(comment, imageId) {
-        this.id = this.constructor.id++;
-        this.comment = comment;
-        this.image = this.findImage(imageId);
-        this.constructor.all.push(this);
-    }
+  constructor(comment, imageId) {
+    this.id = this.constructor.id++;
+    this.comment = comment;
+    this.image = this.findImage(imageId);
+    this.constructor.all.push(this);
+  }
 
-    // find an image object by id, add comment to it
-    // and return the image object
-    findImage(imageId) {
-        const image = Image.all.find(image => image.id === imageId);
-        image.comments.push(this);
-        return image;
-    }
+  // find an image object by id, add comment to it
+  // and return the image object
+  findImage(imageId) {
+    const image = Image.all.find(image => image.id === imageId);
+    image.comments.push(this);
+    return image;
+  }
 
-    // create comment element from current comment object
-    commentEl() {
-        return `<li id="comment-${this.id}">
-            ${this.comment}
-        </li>`;
-    }
+  // create comment element from current comment object
+  commentEl() {
+  return `<li id="comment-${this.id}">
+      ${this.comment}
+  </li>`;
+  }
 }
 
 Comment.all = [];
