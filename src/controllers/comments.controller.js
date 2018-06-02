f34abb4224e11878f0d38633b02332c0a07a2cf6
NMarcora <nicolas.marcora@yahoo.co.uk>
2018-06-02 20:21:52 +0100

    add addCommentFormListener method to append comment when submitted

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -7,15 +7,18 @@ class CommentsController {
     // kick off controller from here
   }
 
-  // add a listener to a form element
+  // add a listener to all form elements
   // to create and render a new comment when submitted
   addCommentFormListener() {
-    form.addEventListener('submit', event => {
-      event.preventDefault();
-      const commentText = event.target.querySelector('.user-text').value;
-      const imageId = parseInt(event.target.dataset.id);
-      const comment = new Comment(commentText, imageId);
-      this.render(comment.commentEl(), comment.image);
-    });
+    const forms = document.querySelectorAll('.add-comment')
+    for (const form of forms) {
+      form.addEventListener('submit', event => {
+        event.preventDefault();
+        const imageId = parseInt(event.target.dataset.id);
+        const commentText = event.target.querySelector('.user-text').value;
+        const commentObject = new Comment(commentText, imageId);
+        this.render(commentObject);
+      });
+    }
   }
 }
