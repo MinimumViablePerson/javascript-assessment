52df87986eb4cc1bf753e933bd9d27a34655df94
NMarcora <nicolas.marcora@yahoo.co.uk>
2018-06-02 20:58:32 +0100

    add findImage method which gets image object by id and appends comment

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,10 +1,19 @@
 class Comment {
     constructor(comment, imageId) {
-        this.id = this.constructor.all.length;
+        this.id = this.constructor.id++;
         this.comment = comment;
         this.image = this.findImage(imageId);
         this.constructor.all.push(this);
     }
+
+    // find an image object by id, add comment to it
+    // and return the image object
+    findImage(imageId) {
+        const image = Image.all.find(image => image.id === imageId);
+        image.comments.push(this);
+        return image;
+    }
 }
 
 Comment.all = [];
+Comment.id = 0;
